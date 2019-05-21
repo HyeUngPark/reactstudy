7f3a8fa610131d072481b2c1919873f45bfb6db6
HyeUngPark <phu8460@gmail.com>
2019-05-22 00:36:56 +0900

    life cycle function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Repeat from './component/Repeat';
+import LifeCycle from './component/LifeCycle';
+
+function getRandomColor(){
+  return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color : '#000000'
+  }
+  handleClick = () =>{
+    this.setState({
+      color : getRandomColor()
+    });
+  }
   render() {
     return (
       <div>
-        <Repeat/>
+        <button onClick={this.handleClick}>Random Color</button>
+        <LifeCycle color={this.state.color}/>
       </div>
     );
   }
