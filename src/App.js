aea3f3937d8a7327497c213c2581ab47a5a09940
HyeUngPark <phu8460@gmail.com>
2019-05-26 16:46:21 +0900

    css test

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import LifeCycle from './component/LifeCycle';
+import styles from './App.css';
 
-function getRandomColor(){
-  return '#'+Math.floor(Math.random()*16777215).toString(16);
-}
+console.log(styles);
 
 class App extends Component {
-  state = {
-    color : '#000000'
-  }
-  handleClick = () =>{
-    this.setState({
-      color : getRandomColor()
-    });
-  }
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>Random Color</button>
-        <LifeCycle color={this.state.color}/>
-      </div>
-    );
-  }
+    render() {
+        return ( 
+            <div className={styles.box}>
+
+            </div>
+        );
+    }
 }
 
 export default App;
