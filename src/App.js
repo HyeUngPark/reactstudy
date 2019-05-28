bbee9508d0a64630f0e04b4f0b72c8ea7c01921b
HyeUngPark <phu8460@gmail.com>
2019-05-28 21:39:57 +0900

    component styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+// import styles from './App.css';
 import styles from './App.css';
-
-console.log(styles);
+import classNames from 'classnames';
+// console.log(styles);
 
 class App extends Component {
     render() {
         return ( 
-            <div className={styles.box}>
-
+/*
+             <div className="blue box">
+ */
+            <div className={classNames(styles.blue, styles.box)}>
+                test
             </div>
         );
     }
